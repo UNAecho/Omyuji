a80c75f1b650419a025ee7a6c25446199fb276b6
andi.you <yadhr582855555@126.com>
2019-08-05 10:12:41 +0800

    Demonstration project

--- Test.py
+++ Test.py
@@ -5,4 +5,5 @@ import random
 # fosterCare.check_time_and_foster("yuhun")
 # if identifyImg.identify_find_template_or_not("boundary_index.png", 0.8):
 #     print(1)
+
 print(win32api.GetCursorPos())
